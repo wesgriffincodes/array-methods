f56b2c98adff844456103d52a63c5485ef077031
Wes Griffin <themisguidedidiot@gmail.com>
2019-06-25 11:17:33 -0700

    created findIndex function and jest test

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const arr = [2,4,6,7,8];
+const arr = [2, 4, 6, 7, 8];
 
 function map(arr, callback) {
-    const mapped = [];
-    for (let i = 0; i < arr.length; i++) {
-        mapped[i] = callback(arr[i]);
-    };
-    return mapped;
-};
+  const mapped = [];
+  for(let i = 0; i < arr.length; i++) {
+    mapped[i] = callback(arr[i]);
+  }
+  return mapped;
+}
 
 const newArray = map(arr, (num) => num * 2);
 
+
 function filter(arr, callback){
-    const filtered = [];
-    for (let i = 0; i < arr.length; i++) {
-      if(callback(arr[i])) {
-        filtered[filtered.length] = arr[i];
-      }
-    };
-    return filtered;
-};
+  const filtered = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      filtered[filtered.length] = arr[i];
+    }
+  }
+  return filtered;
+}
 
-const filteredArray = filter(arr, (num) => num % 2 === 0)
+const filteredArray = filter(arr, (num) => num % 2 === 0);
 
-module.exports = { map, filter}
+
+
+function findIndex(arr, callback) {
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])){
+      return i;
+    }
+  }
+  return -1;
+}
+
+const indexNumber = findIndex(arr, (num) => num > 3);
+
+console.log(indexNumber);
+
+
+module.exports = { map, filter, findIndex };
--- index.test.js
+++ index.test.js
@@ -1,17 +1,24 @@
-const { map, filter } = require('./index');
+const { map, filter, findIndex } = require('./index');
 
-const arr = [2,4,6,7,8];
+const arr = [2, 4, 6, 7, 8];
 
 describe('creates a mapped array function', () => {
-    it('creates a new mapped array', () => {
-        const newArray = map(arr, (num) => num * 2);
-        expect(newArray).toEqual(expect.any(Array));
-    });
+  it('creates a new mapped array', () => {
+    const newArray = map(arr, (num) => num * 2);
+    expect(newArray).toEqual(expect.any(Array));
+  });
 });
 
 describe('create filter function', () => {
-    it('creates a filtered array', () => {
-        const newArray = filter(arr, (num) => num % 2 === 0);
-        expect(newArray).toEqual([2,4,6,8]);
-    });
+  it('creates a filtered array', () => {
+    const newArray = filter(arr, (num) => num % 2 === 0);
+    expect(newArray).toEqual([2, 4, 6, 8]);
+  });
+});
+
+describe('create findIndex function', () => {
+  it('finds item in index', () => {
+    const indexNumber = findIndex(arr, (num) => num > 3);
+    expect(indexNumber).toEqual(1);
+  });
 });
